34442b779bdc42c52f6ceb36ca72d81fcdbe13e2
Bogdan Yatsenko <bogdan.yatsenko.dev@gmail.com>
2025-10-16 14:30:57 +0300

    Delete email-campaign-manager-flask directory

--- email-campaign-manager-flask/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-[ ! -f .env ] && cp .env.example .env || true
-python app.py
